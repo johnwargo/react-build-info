be1b6e245943a07720091a039c22576739a33a52
John M. Wargo <johnwargo@users.noreply.github.com>
2021-01-27 08:47:06 -0500

    Delete buildInfo.js

--- buildInfo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildInfo = void 0;
-exports.buildInfo = {
-    buildVersion: "0.0.7",
-    buildDate: 1611751534805,
-};
